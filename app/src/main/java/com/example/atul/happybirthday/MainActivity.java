0833c098d432353c179c551ca4db0b36987d5a55
Atul Sharma <theatsharma@gmail.com>
2016-06-18 01:26:59 +0530

    Add happy birthday android app

---
+++ app/src/main/java/com/example/atul/happybirthday/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.atul.happybirthday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
